2ce37dc93dbb1f5b5902aee16fd48f3d68ed081a
Michael Maragos <maragosm@aol.com>
2022-07-09 18:37:47 -0400

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class Wordsmith
+    {
+        public bool PalindromeTester(string word)
+        {
+            var reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed += word[i].ToString().ToLower();
+            }
+            if (reversed == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,21 @@
 using System;
 using Xunit;
 
-namespace PalindromeExerciseTests
+namespace PalindromeExercise.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Michael", false)]
+        [InlineData("kayak", true)]
+        [InlineData("madam", true)]
+        [InlineData("mAdam", true)]
+        public void PalindromeTester(string word, bool expected)
         {
-
+            var tester = new Wordsmith();
+            var actual = tester.PalindromeTester(word);
+            Assert.Equal(expected, actual);
         }
     }
 }
